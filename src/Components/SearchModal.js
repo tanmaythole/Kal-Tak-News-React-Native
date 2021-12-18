bfe4da864cb63e87b3379516fa669146b4ae8029
Tanmay <tanmaythole@gmail.com>
2021-12-18 19:09:57 +0530

    search history added in AsyncStorage

--- src/Components/SearchModal.js
+++ src/Components/SearchModal.js
@@ -1,13 +1,42 @@
-import React, { useState } from 'react'
-import { Keyboard, Modal, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { Keyboard, Modal, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import globalStyles from '../styles/GlobalStyles'
 import {MaterialIcons} from '@expo/vector-icons';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export default function SearchModal({modalVisible, setModalVisible}) {
-    const [searchInput, setSearchInput] = useState("");
+    const asyncStorageKey = "@recentSearches-kal-tal-news";
 
-    const SearchQuery = () => {
-        console.log(searchInput)
+    const [searchInput, setSearchInput] = useState("");
+    const [recentResults, setRecentResults] = useState([]);
+
+    const SearchQuery = async () => {
+        const newResults = [...recentResults, searchInput]
+        AsyncStorage.setItem(asyncStorageKey, JSON.stringify(newResults)).then(() => {
+            setRecentResults(newResults);
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+
+    const getSearches = async () => {
+        AsyncStorage.getItem(asyncStorageKey).then((val) => {
+            if(val!==null){
+                setRecentResults(JSON.parse(val));
+            }
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+    useEffect(() => {
+        getSearches()
+    }, [modalVisible])
+
+    const handleClear = async (index) => {
+        const newResults = recentResults.filter((item, ind) => { return ind!==index })
+        AsyncStorage.setItem(asyncStorageKey, JSON.stringify(newResults)).then(() => {
+            setRecentResults(newResults);
+        })
     }
 
     return (
@@ -40,7 +69,19 @@ export default function SearchModal({modalVisible, setModalVisible}) {
 
                 <View style={styles.container}>
                     <Text style={styles.topicTitle}>Recent Searches</Text>
-
+                    {recentResults.map((e, index) => {
+                        return (
+                            <View style={styles.item} key={index}>
+                                <Text style={{color: "#fff"}}>{e}</Text>
+                                <MaterialIcons
+                                    name='clear'
+                                    size={12}
+                                    style={styles.clearIcon}
+                                    onPress={() => handleClear(index)}
+                                />
+                            </View>
+                        )
+                    })}
                 </View>
             </View>
         </Modal>
@@ -80,5 +121,17 @@ const styles = StyleSheet.create({
         color: "#7a7a7a",
         fontSize: 14,
         textTransform: "uppercase"
+    },
+    item: {
+        backgroundColor: "#5c5c5c",
+        padding: 10,
+        marginVertical: 5,
+        borderRadius: 5,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    clearIcon: {
+        color:"#e0e0e0"
     }
 })
